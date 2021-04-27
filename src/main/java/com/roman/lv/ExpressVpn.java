2b706a04b374a23f082d47b44b361eafbf26f6f5
Roman Shtekelman <shtek@yahoo.com>
2021-04-27 14:59:09 +0100

    vpn time out

--- src/main/java/com/roman/lv/ExpressVpn.java
+++ src/main/java/com/roman/lv/ExpressVpn.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class ExpressVpn implements  VpnFactory{
@@ -31,9 +32,13 @@ public class ExpressVpn implements  VpnFactory{
      //becasue it is 4 characters identify the vpn
      String vpnServer =  randomStringFromSet().substring(0,4);
         try {
-            Process process = Runtime.getRuntime().exec("expressvpn connect " + vpnServer );
+            ProcessBuilder builder = new ProcessBuilder("expressvpn connect " + vpnServer);
+            Process process = builder.start();
+             //       Process process = Runtime.getRuntime().exec("expressvpn connect " + vpnServer );
             printResults(process);
-        } catch (IOException e) {
+            process.waitFor(10, TimeUnit.SECONDS);
+
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -47,9 +52,12 @@ public class ExpressVpn implements  VpnFactory{
     @Override
     public void stopVPN() {
         try {
-            Process process = Runtime.getRuntime().exec("expressvpn disconnect");
+            ProcessBuilder builder = new ProcessBuilder("expressvpn disconnect");
+            Process process = builder.start();
+            process.waitFor(10, TimeUnit.SECONDS);
+           // Process process = Runtime.getRuntime().exec("expressvpn disconnect");
             printResults(process);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
